7997364d8fe33b537157e6b6992ca4b1a0c84fab
Steven Rutherford <steve.rutherford12@gmail.com>
2019-10-05 09:13:25 +0100

    start with nothing - initial commit

--- scenes/game.js
+++ scenes/game.js
@@ -1,7 +1,7 @@
 Crafty.defineScene("Game", function() {
 	
 	var redSquare = Crafty.e('2D, DOM, Color')
-		.attr({x: 0, y: 0, w: 100, h: 100})
+		.attr({x: 0, y: 0, w: 16, h: 16})
 		.color('#F00');
 
 });
