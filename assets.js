844ee1b5a4afe9aabb76b2648031ac4d20893bb0
Steven Rutherford <steve.rutherford12@gmail.com>
2019-10-05 14:23:09 +0100

    added full tile image

--- assets.js
+++ assets.js
@@ -10,5 +10,10 @@ var gameAssets = {
             "tileh": 48,
             "map": { "maintile": [0,0] }
         },
+        "assets/images/tilefull48x48.png": {
+            "tile": 48,
+            "tileh": 48,
+            "map": { "fulltile": [0,0] }
+        },
     },
 };
