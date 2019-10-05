a5094c0208b1402666728c694f1b9d2893f231f2
Steven Rutherford <steve.rutherford12@gmail.com>
2019-10-05 09:37:22 +0100

    circle setup and basic asset load

--- assets.js
+++ assets.js
@@ -1,3 +1,14 @@
 
 // 
-var gameAssets = {};
+var gameAssets = {
+    "audio": {
+    },
+    "images": [],
+    "sprites": {
+        "assets/images/tile48x48.png": {
+            "tile": 48,
+            "tileh": 48,
+            "map": { "maintile": [0,0] }
+        },
+    },
+};
--- scenes/game.js
+++ scenes/game.js
@@ -1,7 +1,7 @@
 Crafty.defineScene("Game", function() {
 	
-	var redSquare = Crafty.e('2D, DOM, Color')
-		.attr({x: 0, y: 0, w: 16, h: 16})
+	var redSquare = Crafty.e('2D, DOM, Color, maintile')
+		.attr({x: 0, y: 0, w: 48, h: 48})
 		.color('#F00');
 
 });
