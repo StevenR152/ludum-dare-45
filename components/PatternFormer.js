5043eb8a48f7374cf582888382230d266d84e6d1
Steven Rutherford <steve.rutherford12@gmail.com>
2019-10-05 17:26:43 +0100

    not great, but partial matching - some non-unique check and loss of data

--- components/PatternFormer.js
+++ components/PatternFormer.js
@@ -1,25 +1,42 @@
 Crafty.c("PatternFormer", {
-	_pattern : [[1]],
+	_gridSize : 30,
+	_pattern : [],
 	px : 0,
 	py : 0,
+
+	init : function () {
+		this.px = this._gridSize / 2;
+		this.py = this._gridSize / 2;
+		for (var i = 0; i < this._gridSize; i++) {
+			var rows = [];
+			for (var k = 0; k < this._gridSize; k++) {
+				rows.push(0)
+			}
+			this._pattern.push(rows);
+		}
+		this._pattern[this.py][this.px] += 1;
+		console.log(this._pattern)
+	},	
 
 	recordAction : function (x, y) {
 		console.log("pos", {px:this.px, py:this.py}, "action", {x:x, y:y})
 		this.px += x; this.py += y;
-
-		if(y == -1 && this.py < 0 && Math.abs(this.py) <= this._pattern.length) {
-			// Up, but off the top of the array, to expand it.
-			this._pattern.unshift([1])
-			this.py = 0;
-		} else if(y == 1 && this.py >= this._pattern.length) {
-			// Down but off the end of the array so expand it.
-			this._pattern.push([1])
-			this.py += 1;
-		} else if(this.py >= 0 && this.py < this._pattern.length) {
-			// A movement within the bounds of the array, set counter to 2 because now its solid tile.
-			this._pattern[this.py][this.px] = 2;
-		}
-
+		this._pattern[this.py][this.px] += 1;
+		
 		console.log(this._pattern)
+		console.log(this.simplify())
+	},
+
+	simplify : function () {
+		var newArray = [];
+		for (var i = 0; i < this._pattern.length; i++) {
+			var row = this._pattern[i];
+			var newRow = []
+			for(let i of row)
+   				i && newRow.push(i);
+   			if(newRow.length > 0)
+   				newArray.push(newRow)
+		}
+		return newArray
 	}
 })
