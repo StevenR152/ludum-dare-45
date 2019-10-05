a2935bd6162cf266aa54cd5b2c358ac9545b0e5c
Steven Rutherford <steve.rutherford12@gmail.com>
2019-10-05 10:57:44 +0100

    removed red square from view

--- scenes/game.js
+++ scenes/game.js
@@ -6,14 +6,13 @@ function create_tile(x, y) {
 Crafty.defineScene("Game", function() {
 	var redSquare = create_tile(GAME_SCREEN_X_MIDDLE_IN_TILES, GAME_SCREEN_Y_MIDDLE_IN_TILES);
 
-	var generator = Crafty.e("2D, DOM, Color, Keyboard")
+	var generator = Crafty.e("2D, DOM, Keyboard")
 		.attr({
 			x: GAME_SCREEN_X_MIDDLE_IN_TILES * TSIZE_X,
 			y: GAME_SCREEN_Y_MIDDLE_IN_TILES * TSIZE_Y,
 			w: TSIZE_X / 5,
 			h: TSIZE_Y / 5
 		})
-		.color("red")
 		.bind('KeyDown', function(e) {
 			if (e.key == Crafty.keys.LEFT_ARROW) {
 				this.x -= TSIZE_X;
