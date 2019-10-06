a69f65ead278a2294efb9ab22886c8aac510b042
Steven Rutherford <steve.rutherford12@gmail.com>
2019-10-06 21:07:16 +0100

    remove loading text

--- scenes/Start.js
+++ scenes/Start.js
@@ -2,13 +2,6 @@ Crafty.defineScene("Start", function () {
 	Crafty.background("#38353B");
     Crafty.e("StartBackground") 
   
-    Crafty.e("2D, DOM, Text")
-          .attr({ w: 0, h: 50, x: 270, y: 120 })
-          .text("Loading...")
-          .textFont({ size: '20px', weight: 'bold' })
-          .textAlign("center")
-          .textColor("#111");
- 	
     // Game Assets would be a list of images, but this tutorial doesn't use them.
   	Crafty.load(gameAssets, function(){
        setTimeout(function () {
